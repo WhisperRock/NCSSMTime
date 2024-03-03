f70f2ab08b96a62a46c36485cc7678d79b5356f5
Brandon Yang <143979814+WhisperRock@users.noreply.github.com>
2024-03-03 18:27:55 -0500

    Update classtime.js

--- classtime.js
+++ classtime.js
@@ -1,7 +1,7 @@
 let pageTitle = "";
 let scheduleMap = new Map();
 
-let mod = false;
+let mod = true;
 /*
 If you are reading this, why hello there! 
 I am writing this because I have to remind myself what to do every time there's a modified schedule
@@ -173,7 +173,7 @@ function getNextEvent(dateTime) { // finds the next event
     let events;
     if (mod) { // override
         events = scheduleMap.get("Modified");
-        document.getElementById("banner").innerText = day + ` (Friday Schedule)`; // MODIFY, delete if needed
+        document.getElementById("banner").innerText = day + ` (Senior Photo Schedule)`; // MODIFY, delete if needed
     }
     else {
         events = scheduleMap.get(day);
@@ -192,83 +192,103 @@ function updateTimeMap(currentTime) { // the actual code
     let month = currentTime.getMonth();
     let day = currentTime.getDate();
     scheduleMap.set("Modified", [{
-            date: new Date(year, 1, 25, 22, 0),
+            date: new Date(year, 2, 3, 22, 0),
             name: "before Check"
         },
         {
-            date: new Date(year, 1, 25, 22, 5),
+            date: new Date(year, 2, 3, 22, 5),
             name: "of Check"
         },
         {
-            date: new Date(year, 1, 26, 8, 30),
-            name: "before A5"
+            date: new Date(year, 2, 4, 8, 30),
+            name: "before A1"
         },
         {
-            date: new Date(year, 1, 26, 9, 20),
-            name: "of A5"
+            date: new Date(year, 2, 4, 9, 20),
+            name: "of A1"
         },
         {
-            date: new Date(year, 1, 26, 9, 25),
-            name: "of Transition (A5 to C5)"
+            date: new Date(year, 2, 4, 9, 25),
+            name: "of Transition (A1 to B1)"
         },
         {
-            date: new Date(year, 1, 26, 10, 15),
-            name: "of C5"
+            date: new Date(year, 2, 4, 10, 15),
+            name: "of B1"
         },
         {
-            date: new Date(year, 1, 26, 10, 20),
-            name: "of Transition (C5 to D5L)"
+            date: new Date(year, 2, 4, 10, 20),
+            name: "of Transition (B1 to C1)"
         },
         {
-            date: new Date(year, 1, 26, 11, 50),
-            name: "of D5 and D5 Lab"
+            date: new Date(year, 2, 4, 11, 10),
+            name: "of C1"
         },
         {
-            date: new Date(year, 1, 26, 12, 40),
+            date: new Date(year, 2, 4, 11, 15),
+            name: "of Transition (C1 to D1)"
+        },
+        {
+            date: new Date(year, 2, 4, 12, 5),
+            name: "of D1"
+        },
+        {
+            date: new Date(year, 2, 4, 12, 55),
             name: "of Lunch"
         },
         {
-            date: new Date(year, 1, 26, 13, 30),
-            name: "of B5"
+            date: new Date(year, 2, 4, 13, 45),
+            name: "of Sr. Photo / Mtg."
         },
         {
-            date: new Date(year, 1, 26, 13, 35),
-            name: "of Transition (B5 to E5L)"
+            date: new Date(year, 2, 4, 13, 50),
+            name: "of Transition (to E1)"
         },
         {
-            date: new Date(year, 1, 26, 15, 5),
-            name: "of E5 and E5 Lab"
+            date: new Date(year, 2, 4, 14, 40),
+            name: "of E1"
         },
         {
-            date: new Date(year, 1, 26, 16, 0),
-            name: "of Flexible Use Time"
+            date: new Date(year, 2, 4, 14, 45),
+            name: "of Transition (E1 to F1)"
         },
         {
-            date: new Date(year, 1, 26, 18, 15),
+            date: new Date(year, 2, 4, 15, 35),
+            name: "of F1"
+        },
+        {
+            date: new Date(year, 2, 4, 15, 40),
+            name: "of Transition (F1 to G1)"
+        },
+        {
+            date: new Date(year, 2, 4, 16, 30),
+            name: "of G1"
+        },
+        {
+            date: new Date(year, 2, 4, 18, 15),
             name: "before H1"
         },
         {
-            date: new Date(year, 1, 26, 19, 55),
+            date: new Date(year, 2, 4, 19, 55),
             name: "of H1"
         },
         {
-            date: new Date(year, 1, 26, 20, 5),
+            date: new Date(year, 2, 4, 20, 5),
             name: "of Transition (H1 to I1)"
         },
         {
-            date: new Date(year, 1, 26, 21, 45),
+            date: new Date(year, 2, 4, 21, 45),
             name: "of I1"
         },
         {
-            date: new Date(year, 1, 26, 22, 0),
+            date: new Date(year, 2, 4, 22, 0),
             name: "before Check"
         },
         {
-            date: new Date(year, 1, 26, 22, 5),
+            date: new Date(year, 2, 4, 22, 5),
             name: "of Check"
         },
         {
-            date: new Date(year, 1, 27, 8, 30),
+            date: new Date(year, 2, 5, 8, 30),
             name: "before D2"
         }
     ]);
